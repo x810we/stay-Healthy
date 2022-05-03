38e2244c2c7c212a56d6752de718dcc0216db5f2
IW <ingo.weyck@t-online.de>
2022-05-03 20:43:44 +0200

    first commit

---
+++ android/app/src/main/kotlin/de/iwconsulting/stay_healthy/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.iwconsulting.stay_healthy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/debug/de/iwconsulting/stay_healthy/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.iwconsulting.stay_healthy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "de.iwconsulting.stay_healthy";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
